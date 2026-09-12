396d769a874da194cbf257b26565eeca3985d9a8
Dan Lew <danlew42@gmail.com>
2026-09-12 14:17:14 -0500

    Updated to tzdata 2026dgtz

--- library/src/main/java/net/danlew/android/joda/JodaTimeAndroid.java
+++ library/src/main/java/net/danlew/android/joda/JodaTimeAndroid.java
@@ -7,7 +7,7 @@ import android.content.Context;
  */
 public final class JodaTimeAndroid {
 
-    public static String TZ_DATA_VERSION = "2026cgtz";
+    public static String TZ_DATA_VERSION = "2026dgtz";
 
     private JodaTimeAndroid() {
         // no instances
